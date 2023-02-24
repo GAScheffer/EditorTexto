2591557cbcad3bfd4a8881a700329e1eed9b48e0
Gustavo Scheffer <gustavoascheffer@gmail.com>
2023-02-24 15:57:21 -0300

    Adicionado inputs do prefixo e sufixo. Adicionado input do local do arquivo a ser aberto e salvo.

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import os
+
+# local do arquivo a ser alterado:
+local = input(" Digite o caminho completo da pasta onde está o arquivo: ")
+
+# Quais são os Prefixos e os Sufixos
+prefixo = input(f'Digite os caracteres que ficarão ANTES das palavras já existente no arquivo: ')
+sufixo = input(f'digite os caracteres que ficarão DEPOIS das palavras já existentes no arquivo: ')
+
+
 # abrir o arquivo de texto original para leitura
-with open("/home/gustavo/Área de Trabalho/exemplo.txt", "r") as arquivo_original:
+with open(os.path.join(pasta, "documento.txt"), "r") as arquivo_original:
     # ler o conteúdo do arquivo em uma variável
     conteudo = arquivo_original.read()
 
@@ -10,14 +20,15 @@ palavras = conteudo.split()
 palavras_modificadas = []
 
 # percorrer a lista de palavras e adicionar os caracteres necessários
+
 for palavra in palavras:
-    palavra_modificada = "AAA" + palavra + "XXX" + "\n"
+    palavra_modificada = prefixo + palavra + sufixo + "\n"
     palavras_modificadas.append(palavra_modificada)
 
 # juntar as palavras modificadas de volta em uma string
 novo_conteudo = " ".join(palavras_modificadas)
 
 # abrir um novo arquivo de texto para escrita
-with open("/home/gustavo/Área de Trabalho/resultado.txt", "w") as arquivo_modificado:
+with open(os.path.join(pasta, "resultado.txt"), "w") as arquivo_modificado:
     # escrever o conteúdo modificado no novo arquivo
     arquivo_modificado.write(novo_conteudo)
