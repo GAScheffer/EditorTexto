2877a6f75556fe2e128c3f6a7fcfa20039c610f2
Gustavo Scheffer <gustavoascheffer@gmail.com>
2023-02-26 12:25:00 -0300

    Adicionado textos de orientação para execução do script.

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import os
 
-# local do arquivo a ser alterado:
-local = input(" Digite o caminho completo da pasta onde está o arquivo: ")
+# LOCAL DO ARQUIVO A SER ALTERADO:
+"""
+Escolha qual melhor forma deseja proceder. Pode definir como forma de input e
+sempre digitar o caminho quando executar o script ou deixa o caminho como forma fixa.
+Aquele que desejar, remova a cerquilha da frente: 
+"""
+# local = input(" Digite o caminho completo da pasta onde está o arquivo com o nome do arquivo: ")
+# local = "/home/gustavo/Área de Trabalho/exemplo.txt"
 
-# Quais são os Prefixos e os Sufixos
+
+# INPUT DOS PREFIXOS E DOS SUFIXOS:
 prefixo = input(f'Digite os caracteres que ficarão ANTES das palavras já existente no arquivo: ')
 sufixo = input(f'digite os caracteres que ficarão DEPOIS das palavras já existentes no arquivo: ')
 
-
-# abrir o arquivo de texto original para leitura
+# ABRIR O ARQUIVO DE TEXTO ORIGINAL PARA LEITURA.
+"""
+Na linha 21, altere o nome do "documentos.txt" com o nome do arquivo que será carregado
+"""
 with open(os.path.join(local, "documento.txt"), "r") as arquivo_original:
     # ler o conteúdo do arquivo em uma variável
     conteudo = arquivo_original.read()
@@ -20,7 +29,6 @@ palavras = conteudo.split()
 palavras_modificadas = []
 
 # percorrer a lista de palavras e adicionar os caracteres necessários
-
 for palavra in palavras:
     palavra_modificada = prefixo + palavra + sufixo + "\n"
     palavras_modificadas.append(palavra_modificada)
